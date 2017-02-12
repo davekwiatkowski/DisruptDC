7fb5887a32f6da6cc75a6d83f51ba9706c6d8386
stephentuso <tusodev@gmail.com>
2017-02-12 02:06:07 -0500

    Show events if already logged in

--- src/js/main.js
+++ src/js/main.js
@@ -1,45 +1,6 @@
 (function ($) {
     "use strict";
 
-    function injectPage(url) {
-        var CONTAINER_SELECTOR = '.content';
-
-        //Show loading indicator
-        document.querySelector(CONTAINER_SELECTOR).innerHTML = '<center>Loading...</center>';
-
-        url = decodeURI(url);
-
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', url);
-        xhr.responseType = 'document';
-        xhr.overrideMimeType("text/html; charset=utf-8");
-        xhr.onloadend = function (e) {
-
-            if (e.target.status != 200) {
-                // error handling
-                return;
-            }
-
-            var injectDoc = e.target.response;
-
-            var meta = injectDoc.head.querySelector('meta[itemprop="name"]');
-            if (meta) {
-                var metaContentName = injectDoc.head.querySelector('meta[itemprop="name"]').content;
-                document.head.querySelector('meta[itemprop="name"]').content = metaContentName;
-            }
-
-            // Inject article body.
-            var container = document.querySelector(CONTAINER_SELECTOR);
-            var newDocContainer = injectDoc.querySelector(CONTAINER_SELECTOR);
-            $(container).html(newDocContainer.innerHTML);
-
-            // Scroll to top of page
-            window.scrollTo(0, 0);
-        };
-
-        xhr.send();
-    }
-
     function showBurger() {
         $('#burger').addClass('shown');
         $('#no-burger').removeClass('shown');
@@ -73,6 +34,9 @@
         // main
         handleResize();
 
+        Parse.initialize('disruptDC');
+        Parse.serverURL = 'http://52.33.25.43/parse/';
+
         $('#burger').on('click', handleBurgerClick);
 
         $('.profile-select').on('click', function () {
@@ -87,10 +51,12 @@
             injectPage('chat.html')
         });
 
-        injectPage('login.html');
+        if (Parse.User.current()) {
+            injectPage('events.html');
+        } else {
+            injectPage('login.html');
+        }
 
-        Parse.initialize('disruptDC');
-        Parse.serverURL = 'http://52.33.25.43/parse/';
     }
 
     window.onload = init;
@@ -105,4 +71,43 @@ function onParseReady(func) {
             func();
         })
     }
+}
+
+function injectPage(url) {
+    var CONTAINER_SELECTOR = '.content';
+
+    //Show loading indicator
+    document.querySelector(CONTAINER_SELECTOR).innerHTML = '<center>Loading...</center>';
+
+    url = decodeURI(url);
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.responseType = 'document';
+    xhr.overrideMimeType("text/html; charset=utf-8");
+    xhr.onloadend = function (e) {
+
+        if (e.target.status != 200) {
+            // error handling
+            return;
+        }
+
+        var injectDoc = e.target.response;
+
+        var meta = injectDoc.head.querySelector('meta[itemprop="name"]');
+        if (meta) {
+            var metaContentName = injectDoc.head.querySelector('meta[itemprop="name"]').content;
+            document.head.querySelector('meta[itemprop="name"]').content = metaContentName;
+        }
+
+        // Inject article body.
+        var container = document.querySelector(CONTAINER_SELECTOR);
+        var newDocContainer = injectDoc.querySelector(CONTAINER_SELECTOR);
+        $(container).html(newDocContainer.innerHTML);
+
+        // Scroll to top of page
+        window.scrollTo(0, 0);
+    };
+
+    xhr.send();
 }
